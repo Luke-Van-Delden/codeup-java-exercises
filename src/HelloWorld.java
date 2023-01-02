1a2c17853fb5a74cce19bbfdc7f4329a2c5a2847
Luke Van Delden <vandelden.luke@gmail.com>
2023-01-02 11:26:54 -0600

    beginning java types, syntax, and variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, ");
-        System.out.print("World!");
+        System.out.println("World!");
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
     }
 }
