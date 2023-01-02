2d28001f6a95a344d87ea7346280831db70e3cfe
Luke Van Delden <vandelden.luke@gmail.com>
2023-01-02 11:41:46 -0600

    nearly completed types, syntax, var

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,21 @@ public class HelloWorld {
         System.out.println("World!");
         int myFavoriteNumber = 7;
         System.out.println(myFavoriteNumber);
+        String myString = "viscous";
+        System.out.println(myString);
+        myString = "c";
+        System.out.println(myString);
+        myString = "3.14159";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+
     }
 }
